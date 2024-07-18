a6113f0b763045e556ccff65af921ee1551fc268
alanfinzi <alanfinzi@ultrix.digital>
2024-07-18 13:18:17 -0300

    titulo

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -14,4 +14,10 @@ io.on('connection',clien => {
         console.log('mensajito loco loco',payload);
         io.emit('mensaje',{admin: 'nuevo mensaje'});
     });
+
+    clien.on('emitir-mensaje', ( payload ) => {
+        console.log(payload);
+      // io.emit('nuevo-mensaje', payload); // emite a todos!!
+      clien.broadcast.emit('nuevo-mensaje', payload); //emite a todos mejos al que emitio.
+    })
 })
