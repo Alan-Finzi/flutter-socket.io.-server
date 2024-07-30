b23865cf062ff6ab3fb74ebe57200d98b88c1322
alanfinzi <alanfinzi@ultrix.digital>
2024-07-30 08:29:16 -0300

    actualizacion

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,11 +1,24 @@
-
-
 const { io } = require('../index');
+
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Contenido Cocina' ) );
+bands.addBand( new Band( 'Tutoriales' ) );
+bands.addBand( new Band( 'Entretenimiento' ) );
+bands.addBand( new Band( 'Deporte') );
+bands.addBand( new Band( 'Vida sana') );
+
 
 //Mensaje de sockets
 io.on('connection',clien => {
 
     console.log('cliente conectado');
+
+clien.emit('active-bands',bands.getBands());
+
     clien.on('disconnect', ()=> {
         console.log('cliente desconectado');
     });
@@ -20,4 +33,17 @@ io.on('connection',clien => {
       // io.emit('nuevo-mensaje', payload); // emite a todos!!
       clien.broadcast.emit('nuevo-mensaje', payload); //emite a todos mejos al que emitio.
     })
+
+    clien.on('vote-band', (payload) => {
+
+        bands.voteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+    });
+
+
+    clien.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands() );
+    });
 })
